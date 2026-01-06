39657d1644a50ec11b1b30d382b3f1b7d0a1e3d9
ranlu-bjtu <23120237@bjtu.edu.cn>
2026-01-06 21:34:44 +0800

    Delete RealTimeCommunicationService directory

--- RealTimeCommunicationService/src/main/java/com/wuxi/infinitechat/realtimecommunicationservice/exception/MessageTypeException.java
+++
@@ -1,5 +0,0 @@
-package com.wuxi.infinitechat.realtimecommunicationservice.exception;
-
-public class MessageTypeException extends BaseException{
-    public MessageTypeException(String msg){super(msg);}
-}
--- RealTimeCommunicationService/src/main/java/com/wuxi/infinitechat/realtimecommunicationservice/exception/TokenAuthException.java
+++
@@ -1,7 +0,0 @@
-package com.wuxi.infinitechat.realtimecommunicationservice.exception;
-
-public class TokenAuthException extends BaseException{
-    public TokenAuthException(String msg){
-        super(msg);
-    }
-}
